0c919aac53e7d7d819da1dfa48ce8afea839018d
Pádraig Howlin <padraig@howlin.ie>
2020-04-06 16:02:19 +0100

    feat: show nav and logout buttons when user logs in

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,65 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import { connect } from 'react-redux'
+import { setAuthUser } from '../actions/authedUser'
+import { makeStyles } from '@material-ui/core/styles';
+import { AccountBox } from '@material-ui/icons';
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core'
 
-const NavBar = () => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  accountBox: {
+    marginBottom: -8
+  },
+  welcome: {
+    marginLeft: 50,
+    marginRight: 10,
+    fontStyle: 'italic'
+  }
+}));
+
+const NavBar = (props) => {
+  const classes = useStyles()
+  const { authedUser, users } =  props
+
+  const handleLogout = () => {
+    const { dispatch } = props
+    dispatch( setAuthUser(null) )
+  }
+
   return (
-    <div>
+    <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h5" color="inherit">
+          <Typography variant="h5" className={classes.title}>
             Would You Rather?
           </Typography>
+          {authedUser &&
+            <div>
+                <Button color="inherit">Home</Button>
+                <Button color="inherit">New Question</Button>
+                <Button color="inherit">Leader Board</Button>
+
+                <Typography variant="body1" component="span" className={classes.welcome}> {`Hello ${users[authedUser].name}`} </Typography>
+                <AccountBox color="inherit" className={classes.accountBox} />
+                <Button color="inherit" onClick={handleLogout}>Log Out</Button>
+            </div>
+          }
         </Toolbar>
       </AppBar>
     </div>
   )
 }
 
-export default NavBar
+function mapStateToProps({ authedUser, users }){
+  return {
+    authedUser,
+    users
+  }
+}
+
+export default connect(mapStateToProps)(NavBar)
