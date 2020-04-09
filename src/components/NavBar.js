5dce707b389295915c6840c19418e29c08811f70
Pádraig Howlin <padraig@howlin.ie>
2020-04-09 20:30:54 +0100

    feat: new and improved nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,8 @@ import { connect } from 'react-redux'
 import { setAuthUser } from '../actions/authedUser'
 import { makeStyles } from '@material-ui/core/styles';
 import { AccountBox } from '@material-ui/icons';
-import { AppBar, Toolbar, Typography, Button } from '@material-ui/core'
+import { AppBar, Toolbar, Typography, Grid, Button } from '@material-ui/core'
+import { Link } from 'react-router-dom'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,6 +12,13 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1,
+  },
+  appBarItem: {
+    flexGrow: 1
+  },
+  link: {
+    textDecoration: 'none',
+    color: 'white'
   },
   accountBox: {
     marginBottom: -8
@@ -33,26 +41,33 @@ const NavBar = (props) => {
   }
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h5" className={classes.title}>
-            Would You Rather?
-          </Typography>
-          {authedUser &&
-            <div>
-                <Button color="inherit">Home</Button>
-                <Button color="inherit">New Question</Button>
-                <Button color="inherit">Leader Board</Button>
-
+    <AppBar position="static" className={classes.root}>
+      <Toolbar>
+        {!authedUser 
+          ? <Typography variant="h5" className={classes.title}>
+              Would You Rather?
+            </Typography>
+          : <Grid container>
+              <Grid item align="left" className={classes.appBarItem}>
+                <Button color="inherit">
+                  <Link to="/" className={classes.link}>Home</Link>
+                </Button>
+                <Button color="inherit">
+                  <Link to="/add" className={classes.link}>New Question</Link>
+                </Button>
+                <Button color="inherit">
+                  <Link to="/leaderboard" className={classes.link}>Leader Board</Link>
+                </Button>
+              </Grid>
+              <Grid item align="right" className={classes.appBarItem}>
                 <Typography variant="body1" component="span" className={classes.welcome}> {authUserWelcome} </Typography>
                 <AccountBox color="inherit" className={classes.accountBox} />
                 <Button color="inherit" onClick={handleLogout}>Log Out</Button>
-            </div>
-          }
-        </Toolbar>
-      </AppBar>
-    </div>
+              </Grid>
+            </Grid>
+        }
+      </Toolbar>
+    </AppBar>
   )
 }
 
