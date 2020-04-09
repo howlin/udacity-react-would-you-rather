29e47f758e1dbade738605d5005a74dcca6ef607
Pádraig Howlin <padraig@howlin.ie>
2020-04-09 13:16:48 +0100

    feat: view a question's result

--- src/components/question-types/Result.js
+++ src/components/question-types/Result.js
@@ -1,10 +1,85 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { 
+  Grid,
+  Box,
+  Typography,
+  LinearProgress,
+  Divider } from '@material-ui/core'
+  import { lighten, makeStyles, withStyles } from '@material-ui/core/styles';
+
+  const BorderLinearProgress = withStyles({
+    root: {
+      height: 10,
+      backgroundColor: lighten('#ff6c5c', 0.5),
+    },
+    bar: {
+      borderRadius: 20,
+      backgroundColor: '#ff6c5c',
+    },
+  })(LinearProgress)
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+      margin:20
+    },
+    margin: {
+      margin: theme.spacing(2),
+    },
+    question: {
+      marginTop: 20
+    }
+  }))
 
 const Result = (props) => {
+  const { question } = props
+  const classes = useStyles()
+  const votesOne = question.optionOne.votes.length
+  const votesTwo = question.optionTwo.votes.length
+
   return (
-    <h1>Result</h1>
+    <Grid containerdirection="column">
+      <Box className={classes.root}>
+        <Grid item>
+          <Typography variant="h6">Result:</Typography>
+        </Grid>
+        <Grid item className={classes.question} align="center">
+            <Typography variant="body1">Would you rather {question.optionOne.text}</Typography>
+            <BorderLinearProgress
+              className={classes.margin}
+              variant="determinate"
+              color="secondary"
+              value={(votesOne / (votesOne + votesTwo)) * 100}
+            />
+            <Typography variant="body1">
+              {votesOne} out of {votesOne + votesTwo} votes
+            </Typography>
+        </Grid>
+        <Grid item className={classes.question} >
+          <Divider />
+        </Grid>
+        <Grid item className={classes.question} align="center">
+            <Typography variant="body1">Would you rather {question.optionTwo.text}</Typography>
+            <BorderLinearProgress
+              className={classes.margin}
+              variant="determinate"
+              color="secondary"
+              value={(votesTwo / (votesOne + votesTwo)) * 100}
+            />
+            <Typography variant="body1">
+              {votesTwo} out of {votesOne + votesTwo} votes
+            </Typography>
+        </Grid>
+      </Box>
+    </Grid>
   )
 }
 
-export default connect()(Result)
+function mapStateToProps({ questions }, { qid }){
+  return {
+    question: questions[qid]
+  }
+}
+
+export default connect(mapStateToProps)(Result)
